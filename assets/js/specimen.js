daffd66948ac27c099ea2097417aed70a62f1cca
Petr Novotný <krkabol@gmail.com>
2025-03-17 09:11:00 +0100

    resolve GBIF detial better

--- assets/js/specimen.js
+++ assets/js/specimen.js
@@ -1,4 +1,11 @@
-export default async function makeJACQLinks() {
+export default async function joinLinks() {
+    makeJACQLinks();  
+    makeGbifLink();
+}
+
+
+
+async function makeJACQLinks() {
 
     const pidElement = document.getElementById("PID");
     if (!pidElement) {
@@ -41,6 +48,44 @@ export default async function makeJACQLinks() {
             manifestLinkElement.href = `${apiUrlManifest}${data.specimenID}`;
             miradorElement.setAttribute('data-manifest', `${apiUrlManifest}${data.specimenID}`);
         }
+
+    } catch (error) {
+    }
+
+}
+
+async function makeGbifLink() {
+
+    const gbifElement = document.getElementById("GBIF");
+    const gbifInfoElement = document.getElementById("copyGbif");
+    if (!gbifElement) {
+        return;
+    }
+
+    const apiUrl = gbifElement.getAttribute("data-url");
+    if (!apiUrl) {
+        return;
+    }
+
+    try {
+        const response = await fetch(apiUrl, {
+            headers: {"Accept": "application/json"}
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (Array.isArray(data.results) && data.results.length > 0) {
+            let firstKey = data.results[0].key;            gbifElement.href = `https://gbif.org/occurrence/${firstKey}`;
+            gbifInfoElement.textContent = `https://gbif.org/occurrence/${firstKey}`;
+          } else {
+            console.error("missing response");
+            return;
+          }
+         
 
     } catch (error) {
     }
