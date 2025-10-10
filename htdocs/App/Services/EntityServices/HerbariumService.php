996ae5b10ccee4521cd61d81dd80af2ff289c828
Petr Novotný <krkabol@gmail.com>
2025-10-10 12:36:47 +0200

    allow set setting only with no unprocessed photos

--- htdocs/App/Services/EntityServices/HerbariumService.php
+++ htdocs/App/Services/EntityServices/HerbariumService.php
@@ -3,12 +3,14 @@
 namespace App\Services\EntityServices;
 
 use App\Model\Database\Entity\Herbaria;
+use App\Model\Database\Entity\Photos;
 use App\Security\Identity;
+use App\Services\Exceptions\RiskOfUnpredictabilityException;
+use Doctrine\ORM\EntityManagerInterface;
 use Nette\Security\User;
 
 class HerbariumService extends BaseEntityService
 {
-
     protected string $entityName = Herbaria::class;
 
     public function getCurrentUserHerbarium(User $user): ?Herbaria
@@ -37,6 +39,10 @@ class HerbariumService extends BaseEntityService
     public function setFilenameFallback(User $user, bool $value): HerbariumService
     {
         $herbarium = $this->getCurrentUserHerbarium($user);
+        $photosRepository = $this->entityManager->getRepository(Photos::class);
+        if (count($photosRepository->findUnprocessedPhotos($user)) > 0){
+            throw new RiskOfUnpredictabilityException('Change this setting is allowed only when having no unprocessed photos.');
+        }
         if ($herbarium) {
             $herbarium->setFallbackFilename($value);
             $this->entityManager->flush();
@@ -47,6 +53,10 @@ class HerbariumService extends BaseEntityService
     public function setMultiplier(User $user, bool $value): HerbariumService
     {
         $herbarium = $this->getCurrentUserHerbarium($user);
+        $photosRepository = $this->entityManager->getRepository(Photos::class);
+        if (count($photosRepository->findUnprocessedPhotos($user)) > 0){
+            throw new RiskOfUnpredictabilityException('Change this setting is allowed only when having no unprocessed photos.');
+        }
         if ($herbarium) {
             $herbarium->setMultipleBarcodeMultiplier($value);
             $this->entityManager->flush();
---
+++ htdocs/App/Services/Exceptions/RiskOfUnpredictabilityException.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+namespace App\Services\Exceptions;
+
+use Nette\Neon\Exception;
+
+class RiskOfUnpredictabilityException extends ServiceException
+{
+
+}
