7be77609e0543b8eaeb2fa241e55ce12c24ac74c
Petr Novotný <krkabol@gmail.com>
2026-01-13 12:48:00 +0100

    fix JACQ specimen check

--- htdocs/tests/Cases/Unit/Model/Database/Entity/PhotosTest.php
+++ htdocs/tests/Cases/Unit/Model/Database/Entity/PhotosTest.php
@@ -107,7 +107,7 @@ test('Photos entity getters and setters', function (): void {
     $photos->setSpecimenId('000123');
     Assert::same('123', $photos->specimenId);
     Assert::same('ABC_000123', $photos->getFullSpecimenId());
-    Assert::same('https://abc.jacq.org/ABC123', $photos->getExpectedJacqPid());
+    Assert::same('https://abc.jacq.org/ABC000123', $photos->getExpectedJacqPid());
 
     // Test původního jména souboru
     $photos->setOriginalFilename('original.jpg');
