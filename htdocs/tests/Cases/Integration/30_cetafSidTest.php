14290f7ead2cd92db1adb4a2ed2c2ef29a237237
Petr Novotný <krkabol@gmail.com>
2026-03-18 09:49:29 +0100

    fix test passing

--- htdocs/tests/Cases/Integration/30_cetafSidTest.php
+++ htdocs/tests/Cases/Integration/30_cetafSidTest.php
@@ -32,7 +32,12 @@ final class CetafSidTest extends IntegrationTestCase
         $managementService = new CetafSidImportService($this->provideLoggedCuratorUser(), $session, $this->em, $service);
         $managementService->import($values);
         Assert::count(4, $service->findAll(), 'imported CETAF sids');
-
+        //hack lastedit_timestamp to pretend the records are "older" and will be processed by the cronjob right now
+        $conn = $this->em->getConnection();
+        $conn->executeStatement("
+                    UPDATE photos
+                    SET lastedit_timestamp = lastedit_timestamp - INTERVAL '1 day'
+                ");
         $this->runCommand(['command' => 'curator:resolveSpecimenPid', '--no-interaction' => true,], 'sid resolve broken');
 
         Assert::count(4, $servicePhotos->findBy(['status'=>PhotosStatus::SPECIMEN_CONTROL_OK]), 'moved to specimen_control_ok using CETAF sids');
