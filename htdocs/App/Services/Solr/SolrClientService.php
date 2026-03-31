264814bfff5ff7a68b026a0970c4a219db9cc8bd
Petr Novotný <krkabol@gmail.com>
2026-03-31 11:10:07 +0200

    fix solr publishing with no cetaf info harvested

--- htdocs/App/Services/Solr/SolrClientService.php
+++ htdocs/App/Services/Solr/SolrClientService.php
@@ -30,6 +30,9 @@ final readonly class SolrClientService
         foreach ($photos as $photo) {
             $doc = $update->createDocument();
             $doc = $this->prepareIngest($photo, $doc);
+            if ($doc === null) {
+                continue;
+            }
             $update->addDocument($doc);
         }
         if ($commit) {
@@ -56,50 +59,50 @@ final readonly class SolrClientService
         dump($request->getRawData());  // payload
     }
 
-    protected function prepareIngest(Photos $photo, DocumentInterface $document): DocumentInterface
+    protected function prepareIngest(Photos $photo, DocumentInterface $document): ?DocumentInterface
     {
         $cetafJson = $photo->getDatabotOkResultById($this->entityManager->getRepository(Databot::class)->getByName(DatabotRepository::CETAF))?->resultData ?? null;
-        if (empty($cetafJson)) {
-            return $document;
-        }
         $document->setField('id', (string)$photo->pid);
 
-        if ($cetafJson !== null) {
-            $document->setField('title', $cetafJson["http://purl.org/dc/terms/title"] ?? null);
-            $document->setField('basis_of_record', 'PreservedSpecimen');
-            $document->setField('herbarium_acronym', strtoupper($photo->herbarium->acronym));
-
-
-            $document->setField('description', 'Photo of a herbarium specimen');
-            $document->setField('locality', $cetafJson['http://rs.tdwg.org/dwc/terms/locality'] ?? null);
-
-            // people
-            $document->setField('creator', $cetafJson['http://purl.org/dc/terms/creator'] ?? null);
-            $document->setField('recorded_by', $cetafJson['http://rs.tdwg.org/dwc/terms/recordedBy'] ?? null);
-
-            // taxonomy
-            $document->setField('scientific_name', $cetafJson['http://rs.tdwg.org/dwc/terms/scientificName'] ?? null);
-            $document->setField('genus', $cetafJson['http://rs.tdwg.org/dwc/terms/genus '] ?? null);
-            $document->setField('family', $cetafJson['http://rs.tdwg.org/dwc/terms/family'] ?? null);
-            $document->setField('specific_epithet', $cetafJson['http://rs.tdwg.org/dwc/terms/specificEpithet'] ?? null);
-
-            // geo
-            $document->setField('country', $cetafJson['http://rs.tdwg.org/dwc/terms/country'] ?? null);
-            $document->setField('country_code', $cetafJson['http://rs.tdwg.org/dwc/terms/countryCode'] ?? null);
-
-            // dates
-            $document->setField('event_date', $cetafJson['http://rs.tdwg.org/dwc/terms/eventDate'] ?? null);
-            $document->setField('created', $cetafJson['dc:created'] ?? null);
-
-            // identifiers
-            $document->setField('catalog_number', $cetafJson['http://rs.tdwg.org/dwc/terms/catalogNumber'] ?? null);
-            $document->setField('collection_code', $cetafJson['http://rs.tdwg.org/dwc/terms/collectionCode'] ?? []);
-
-            // misc
-            $document->setField('material_sample_id', $cetafJson['http://rs.tdwg.org/dwc/terms/materialSampleID '] ?? null);
-
-            $document->setField('previous_identifications', $cetafJson['http://rs.tdwg.org/dwc/terms/previousIdentifications '] ?? []);
+        if ($cetafJson === null) {
+            return null;
         }
+
+        $document->setField('title', $cetafJson["http://purl.org/dc/terms/title"] ?? null);
+        $document->setField('basis_of_record', 'PreservedSpecimen');
+        $document->setField('herbarium_acronym', strtoupper($photo->herbarium->acronym));
+
+
+        $document->setField('description', 'Photo of a herbarium specimen');
+        $document->setField('locality', $cetafJson['http://rs.tdwg.org/dwc/terms/locality'] ?? null);
+
+        // people
+        $document->setField('creator', $cetafJson['http://purl.org/dc/terms/creator'] ?? null);
+        $document->setField('recorded_by', $cetafJson['http://rs.tdwg.org/dwc/terms/recordedBy'] ?? null);
+
+        // taxonomy
+        $document->setField('scientific_name', $cetafJson['http://rs.tdwg.org/dwc/terms/scientificName'] ?? null);
+        $document->setField('genus', $cetafJson['http://rs.tdwg.org/dwc/terms/genus'] ?? null);
+        $document->setField('family', $cetafJson['http://rs.tdwg.org/dwc/terms/family'] ?? null);
+        $document->setField('specific_epithet', $cetafJson['http://rs.tdwg.org/dwc/terms/specificEpithet'] ?? null);
+
+        // geo
+        $document->setField('country', $cetafJson['http://rs.tdwg.org/dwc/terms/country'] ?? null);
+        $document->setField('country_code', $cetafJson['http://rs.tdwg.org/dwc/terms/countryCode'] ?? null);
+
+        // dates
+        $document->setField('event_date', $cetafJson['http://rs.tdwg.org/dwc/terms/eventDate'] ?? null);
+        $document->setField('created', $cetafJson['dc:created'] ?? null);
+
+        // identifiers
+        $document->setField('catalog_number', $cetafJson['http://rs.tdwg.org/dwc/terms/catalogNumber'] ?? null);
+        $document->setField('collection_code', $cetafJson['http://rs.tdwg.org/dwc/terms/collectionCode'] ?? null);
+
+        // misc
+        $document->setField('material_sample_id', $cetafJson['http://rs.tdwg.org/dwc/terms/materialSampleID'] ?? null);
+
+        $document->setField('previous_identifications', $cetafJson['http://rs.tdwg.org/dwc/terms/previousIdentifications'] ?? null);
+
         return $document;
     }
 
