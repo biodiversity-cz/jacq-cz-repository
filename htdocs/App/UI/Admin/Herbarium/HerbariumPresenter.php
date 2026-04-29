3cfc243e0ee6b9d4df087efdf3d02e1efa80b0bf
petr <krkabol@gmail.com>
2026-04-29 07:43:33 +0200

    fix exception in settings

--- htdocs/App/UI/Admin/Herbarium/HerbariumPresenter.php
+++ htdocs/App/UI/Admin/Herbarium/HerbariumPresenter.php
@@ -6,6 +6,7 @@ use App\Model\Database\Entity\Herbaria;
 use App\Security\Identity;
 use App\Services\EntityServices\HerbariumService;
 use App\Services\EntityServices\UserService;
+use App\Services\Exceptions\RiskOfUnpredictabilityException;
 use App\UI\Base\BasePresenter;
 use App\UI\Base\SecuredPresenter;
 use Doctrine\ORM\EntityManagerInterface;
@@ -35,11 +36,15 @@ final class HerbariumPresenter extends SecuredPresenter
         if ($value === 'true'){
             $value = true;
         }
-        if ($this->user->isInRole('curator')){
-        match ($feature) {
-            'filenameFallbackSwitch' => $this->herbariumService->setFilenameFallback($this->user, (bool)$value),
-            'multiplierSwitch' => $this->herbariumService->setMultiplier($this->user, (bool)$value),
-        };
+        try {
+            if ($this->user->isInRole('curator')) {
+                match ($feature) {
+                    'filenameFallbackSwitch' => $this->herbariumService->setFilenameFallback($this->user, (bool)$value),
+                    'multiplierSwitch' => $this->herbariumService->setMultiplier($this->user, (bool)$value),
+                };
+            }
+        }catch (RiskOfUnpredictabilityException $exception){
+            $this->flashMessage($exception->getMessage(), 'warning');
         }
 
         $this->redirect(':default');
