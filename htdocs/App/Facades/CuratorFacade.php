9d1a2e2e428d4c7c00e8176164ad98c83a9553c3
petr <krkabol@gmail.com>
2026-05-07 08:44:25 +0200

    new Solr location

--- htdocs/App/Facades/CuratorFacade.php
+++ htdocs/App/Facades/CuratorFacade.php
@@ -150,7 +150,7 @@ readonly class CuratorFacade
             ->pipe($this->stageFactory->createConvertStage())
             ->pipe($this->stageFactory->createPidStage())
             ->pipe($this->stageFactory->createChecksumStage())
-//            ->pipe($this->stageFactory->createSolrStage())
+            ->pipe($this->stageFactory->createSolrStage())
             ->pipe($this->stageFactory->createTransferJp2Stage())
             ->pipe($this->stageFactory->createCleanupTempFilesStage());
     }
