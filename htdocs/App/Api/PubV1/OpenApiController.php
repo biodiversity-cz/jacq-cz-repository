531b93fd1986b47135ecc2117a3706d98ef9a49b
petr <krkabol@gmail.com>
2026-05-18 15:17:25 +0200

    fix OPTIONS for openapi

--- htdocs/App/Api/PubV1/OpenApiController.php
+++ htdocs/App/Api/PubV1/OpenApiController.php
@@ -23,7 +23,7 @@ class OpenApiController extends BasePubV1Controller
 
     #[Apitte\OpenApi('summary: Get OpenAPI definition.')]
     #[Apitte\Path('/meta')]
-    #[Apitte\Method('GET', 'OPTIONS')]
+    #[Apitte\Method(['GET', 'OPTIONS'])]
     #[Apitte\Response(description: 'Success', code: '200')]
     public function meta(ApiRequest $request, ApiResponse $response): ResponseInterface
     {
